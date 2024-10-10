520b554ebb7d472ad606710b99c2070b8f04c052
bekbolat <bekbolatjabbarbergenov@gmail.com>
2024-10-10 17:58:06 +0500

    firs

--- js/singin.js
+++ js/singin.js
@@ -9,27 +9,43 @@ const post = async (url, data) => {
         });
 
         if (!response.ok) {
-            throw new error("ERROR");
+            throw new Error(`Xato: ${response.status} - ${response.statusText}`);
         }
-        const result = await response.json();
-        return result;
+
+        const result = await response.json(); 
+        return { status: response.status, data: result }; 
     } catch (error) {
-        console.error(error);
+        console.error('Xato:', error.message);
+        return { status: 'error', error: error.message }; 
     }
 };
+
 const form = document.querySelector('form');
-form.addEventListener("submit" , e => {
+form.addEventListener("submit", async (e) => {
     e.preventDefault();
-    const fname = document.getElementById('fname')
-    const lname = document.getElementById('lname')
-    const email = document.getElementById('email')
-    const phone = document.getElementById('phone')
+    const fname = document.getElementById('fname');
+    const lname = document.getElementById('lname');
+    const email = document.getElementById('email');
+    const phone = document.getElementById('phone');
+    const password = document.getElementById('password');
+    const address = document.getElementById('adress');
+    const categories = document.getElementById('categories');
+
     const data = {
-        fname: fname.value,
-        lname: lname.value,
+        first_name: fname.value,
+        last_name: lname.value,
         email: email.value,
         phone: phone.value,
-
+        password: password.value,
+        address: address.value,
+        categories: categories.value,
+    };
+    localStorage.setItem('email', email.value); 
+    localStorage.setItem('password', password.value);
+    localStorage.setItem('categories', categories.value);
+    const url = 'http://192.168.1.3:5000/customers'; 
+    const response = await post(url, data);
+    if (response.status == 201) {
+        location.href = 'index.html';
     }
-    post("http://localhost:3000/users" , data)
-})
+});
